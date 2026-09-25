99fd2d6cebdac642cef4f36a24fc5eeadb09c68d
Wilson-Uchenna <wilsonuchenna08@gmail.com>
2026-09-25 20:39:09 +0100

    made changes on user and admin flow

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3001",
+        "arwpce.name.ng",
+        "www.arwpce.name.ng", // include if www resolves too
+      ],
+    },
+  },
 };
 
 export default nextConfig;
